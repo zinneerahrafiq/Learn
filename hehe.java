bb7d442ee628d6fc5ba1b5c91952851e4b5008cb
jazil10 <syedjazil10@gmail.com>
2024-03-28 15:36:11 +0500

    New commit

--- hehe.java
+++ hehe.java
@@ -3,5 +3,6 @@ package flutter_application_1;
 public class hehe {
     public static void main(String[] args) {
        System.out.println("hello");
+       System.out.println("hello from the other side zinneerah rafiq.");
     }
 }
